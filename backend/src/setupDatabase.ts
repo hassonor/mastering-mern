3b104c5454e3f2433ec350acc5853caa62d4a3c4
Or Hasson <hassonor@gmail.com>
2022-12-17 19:16:37 +0200

    setup base redis cache connect

--- backend/src/setupDatabase.ts
+++ backend/src/setupDatabase.ts
@@ -2,6 +2,7 @@ import * as process from 'process';
 import mongoose from 'mongoose';
 import Logger from 'bunyan';
 import { config } from '@root/config';
+import { redisConnection } from '@service/redis/redis.connection';
 
 const log: Logger = config.createLogger('setupDB');
 export default () => {
@@ -11,6 +12,7 @@ export default () => {
             .connect(`${config.DATABASE_URI}`)
             .then(() => {
                 log.info('Successfully connected to database.');
+                redisConnection.connect();
             })
             .catch((error) => {
                 log.error('Error when connecting to database', error);
