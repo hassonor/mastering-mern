35c5c9a497bb2095bd4a476a2b593388e3f32146
Or Hasson <hassonor@gmail.com>
2022-12-17 16:27:13 +0200

    change absolute path setup to be with ttypescript and decorators

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,20 @@
 import express, { Express } from 'express';
-import { HassonServer } from './setupServer';
-import dbConnection from './setupDatabase';
-import { config } from './config';
+import { HassonServer } from '@root/setupServer';
+import dbConnection from '@root/setupDatabase';
+import { config } from '@root/config';
 
 class Application {
-  public initialize(): void {
-    this.loadConfig();
-    dbConnection();
-    const app: Express = express();
-    const server: HassonServer = new HassonServer(app);
-    server.start();
-  }
+    public initialize(): void {
+        this.loadConfig();
+        dbConnection();
+        const app: Express = express();
+        const server: HassonServer = new HassonServer(app);
+        server.start();
+    }
 
-  private loadConfig(): void {
-    config.validateConfig();
-  }
+    private loadConfig(): void {
+        config.validateConfig();
+    }
 }
 
 const application: Application = new Application();
