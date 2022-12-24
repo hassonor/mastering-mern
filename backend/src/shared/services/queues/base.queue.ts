0fc8830aca3b8424046998b8b0c9c9ded506682c
Or Hasson <hassonor@gmail.com>
2022-12-24 23:20:14 +0200

    adding email worker,queue , reset and forgot password ejs teamplates

--- backend/src/shared/services/queues/base.queue.ts
+++ backend/src/shared/services/queues/base.queue.ts
@@ -4,9 +4,9 @@ import { createBullBoard, ExpressAdapter } from '@bull-board/express';
 import { BullAdapter } from '@bull-board/api/bullAdapter';
 import { config } from '@root/config';
 import { IAuthJob } from '@auth/interfaces/auth.interface';
+import { IEmailJob } from '@user/interfaces/user.interface';
 
-type IBaseJobData =
-    | IAuthJob
+type IBaseJobData = IAuthJob | IEmailJob;
 
 let bullAdapters: BullAdapter[] = [];
 
