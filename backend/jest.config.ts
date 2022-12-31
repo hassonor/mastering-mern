7bb4c3a772d19e5a8f884992478c3b00aeb87dd8
Or Hasson <hassonor@gmail.com>
2022-12-31 15:27:59 +0200

    adding auth controllers tests and ns paths

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -22,13 +22,20 @@ const config: Config.InitialOptions = {
     },
     coverageReporters: ['text-summary', 'lcov'],
     moduleNameMapper: {
-        '@auth/(.*)': ['<rootDir>/src/feature/auth/$1'],
-        '@user/(.*)': ['<rootDir>/src/feature/user/$1'],
+        '@auth/(.*)': ['<rootDir>/src/features/auth/$1'],
+        '@user/(.*)': ['<rootDir>/src/features/user/$1'],
+        '@post/(.*)': ['<rootDir>/src/features/post/$1'],
+        '@reaction/(.*)': ['<rootDir>/src/features/reactions/$1'],
+        '@comment/(.*)': ['<rootDir>/src/features/comments/$1'],
+        '@follower/(.*)': ['<rootDir>/src/features/followers/$1'],
+        '@notification/(.*)': ['<rootDir>/src/features/notifications/$1'],
+        '@image/(.*)': ['<rootDir>/src/features/images/$1'],
+        '@chat/(.*)': ['<rootDir>/src/features/chat/$1'],
         '@global/(.*)': ['<rootDir>/src/shared/globals/$1'],
         '@service/(.*)': ['<rootDir>/src/shared/services/$1'],
         '@socket/(.*)': ['<rootDir>/src/shared/sockets/$1'],
         '@worker/(.*)': ['<rootDir>/src/shared/workers/$1'],
-        '@root/(.*)': ['<rootDir>/src/$1']
+        '@root/(.*)': ['<rootDir>/src/$1'],
     }
 };
 
