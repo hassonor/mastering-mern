18fff735332ceb7194e747405190a0c26f7e0611
Or Hasson <hassonor@gmail.com>
2023-07-24 11:52:24 +0300

    fix vars names

--- backend/src/features/followers/controllers/followerUser.controller.ts
+++ backend/src/features/followers/controllers/followerUser.controller.ts
@@ -22,17 +22,17 @@ export class Add {
         await Promise.all([followersCount, followingCount]);
 
         const cachedFollower: Promise<IUserDocument> = userCache.getUserFromCache(`${followerId}`) as Promise<IUserDocument>;
-        const cachedFollowing: Promise<IUserDocument> = userCache.getUserFromCache(`${req.currentUser!.userId}`) as Promise<IUserDocument>;
-        const response: [IUserDocument, IUserDocument] = await Promise.all([cachedFollower, cachedFollowing]);
+        const cachedFollowedUser: Promise<IUserDocument> = userCache.getUserFromCache(`${req.currentUser!.userId}`) as Promise<IUserDocument>;
+        const response: [IUserDocument, IUserDocument] = await Promise.all([cachedFollower, cachedFollowedUser]);
 
         const followerObjectId: ObjectId = new ObjectId();
-        const addFollowingData: IFollowerData = Add.prototype.userData(response[0]);
+        const addFollowedUserData: IFollowerData = Add.prototype.userData(response[0]);
 
-        socketIOFollowerObject.emit('add follower', addFollowingData);
+        socketIOFollowerObject.emit('add follower', addFollowedUserData);
 
         const addFollowerToCache: Promise<void> = followerCache.saveFollowerToCache(`followers:${req.currentUser!.userId}`, `${followerId}`);
-        const addFollowingToCache: Promise<void> = followerCache.saveFollowerToCache(`following:${followerId}`, `${req.currentUser!.userId}`);
-        await Promise.all([addFollowerToCache, addFollowingToCache]);
+        const addFollowedUserToCache: Promise<void> = followerCache.saveFollowerToCache(`following:${followerId}`, `${req.currentUser!.userId}`);
+        await Promise.all([addFollowerToCache, addFollowedUserToCache]);
 
         // send data to queue
         res.status(HTTP_STATUS.OK).json({message: 'Following user now'});
