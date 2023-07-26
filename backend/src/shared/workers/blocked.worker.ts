06a9eab3ea54785ac3d3b5ded434245cf028795e
Or Hasson <hassonor@gmail.com>
2023-07-26 12:42:37 +0300

    add block service file + implement blocked worker

---
+++ backend/src/shared/workers/blocked.worker.ts
@@ -0,0 +1,28 @@
+import {DoneCallback, Job} from 'bull';
+import Logger from 'bunyan';
+import {config} from '@root/config';
+import {blockUserService} from '@service/db/block.service';
+
+
+const log: Logger = config.createLogger('blockedUserWorker');
+
+class BlockedUserWorker {
+    async addBlockedUserToDB(job: Job, done: DoneCallback): Promise<void> {
+        try {
+            const {keyOne, keyTwo, action} = job.data;
+            if (action === 'block') {
+                await blockUserService.blockUser(keyOne, keyTwo);
+            } else {
+                await blockUserService.unblockUser(keyOne, keyTwo);
+            }
+
+            job.progress(100);
+            done(null, job.data);
+        } catch (error) {
+            log.error(error);
+            done(error as Error);
+        }
+    }
+}
+
+export const blockedUserWorker: BlockedUserWorker = new BlockedUserWorker();
