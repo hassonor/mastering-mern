f2829afa03ec6d4b0270d9c3de71d2f9e29aa852
Or Hasson <hassonor@gmail.com>
2023-08-27 21:37:26 +0300

    add get notifcations controller + route

---
+++ backend/src/features/notifications/controllers/getNotification.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import HTTP_STATUS from 'http-status-codes';
+import { INotificationDocument } from '@notification/interfaces/notification.interface';
+import { notificationService } from '@service/db/notification.service';
+
+export class Get {
+    public async notifications(req: Request, res: Response): Promise<void> {
+        const notifications: INotificationDocument[] = await notificationService.getNotifications(req.currentUser!.userId);
+        res.status(HTTP_STATUS.OK).json({message: 'User notifications', notifications});
+    }
+}
--- backend/src/features/notifications/routes/notificationRoutes.ts
+++ backend/src/features/notifications/routes/notificationRoutes.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { Update } from '@notification/controllers/updateNotification.controller';
 import { Delete } from '@notification/controllers/deleteNotifcation.controller';
+import { Get } from '@notification/controllers/getNotification.controller';
 
 class NotificationRoutes {
     private readonly router: Router;
@@ -11,6 +12,7 @@ class NotificationRoutes {
     }
 
     public routes(): Router {
+        this.router.get('/notifications', authMiddleware.checkAuthentication, Get.prototype.notifications);
         this.router.put('/notification/:notificationId', authMiddleware.checkAuthentication, Update.prototype.notification);
         this.router.delete('/notification/:notificationId', authMiddleware.checkAuthentication, Delete.prototype.notification);
 
